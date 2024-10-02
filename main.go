c70668af91e90e69d5c6aaa3ae3e13c31b317b25
mamata.barman <mamata.barman2@go-mmt.com>
2024-10-03 01:32:27 +0530

    feat: inject dynamic data in html

--- main.go
+++ main.go
@@ -17,10 +17,24 @@ func main() {
 	// 	io.WriteString(w, r.Method)
 	// }
 
+	//For record
+	type Film struct {
+		Title    string
+		Director string
+	}
+
 	//Parse the html and show in browser, data driven templates
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		temp := template.Must(template.ParseFiles("index.html"))
-		temp.Execute(w, nil)
+
+		films := map[string][]Film{
+			"Films": {
+				{Title: "Blade", Director: "Ridley"},
+				{Title: "Godfather", Director: "Francis"},
+			},
+		}
+		//films is the data we want to inject in html dynamically, ultimately html will display this.
+		temp.Execute(w, films)
 	}
 
 	http.HandleFunc("/", h1)
